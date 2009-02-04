fa62152a673c19678420055d958e511799966b8c
unknown <ravi116@gmail.com>
2009-02-04 15:52:09 -0600

    Add example using mockito

---
+++ src/test/java/com/availity/learning/mocktesting/CarePrescribeBPOImplTest.java
@@ -0,0 +1,31 @@
+package com.availity.learning.mocktesting;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnit44Runner;
+
+@RunWith(MockitoJUnit44Runner.class)
+public class CarePrescribeBPOImplTest {
+	
+	@Mock UserDao userDao;
+	@Mock OrganizationDao organizationDao;
+	@Mock PrematicsService prematicsService;
+	
+	
+	@Test
+	public void shouldTestUsingMockitExample() throws PrematicsException{
+		when(userDao.isRegistered("user")).thenReturn(Boolean.TRUE);
+		
+		
+		CarePrescribeBPOImpl carePrescribeService = new CarePrescribeBPOImpl(userDao, 
+												organizationDao, prematicsService);
+		carePrescribeService.sendServiceRequest("user");
+		verify(prematicsService).registerUser("user");
+	}
+
+}
