6a3fb46fc5e94010f376a91a264773ba3b8a02cd
Scott Williams <scott@jeeprofessional.com>
2009-02-07 09:15:03 -0600

    Added coalmineCanary test.

--- src/test/groovy/SecondGroovyClass.groovy
+++ src/test/groovy/SecondGroovyClass.groovy
@@ -1,7 +1,15 @@
+import org.junit.Test
+
 /**
  * @author <a mailto:swilliams@availity.com> Scott Williams</a>
  */
 
 public class SecondGroovyClass {
+
+  @Test
+  void coalMineCanaryTest()
+  {
+    assert true
+  }
 
 }
